d3bb10bf7de2b8b0688c2f965df60efcdca4d9f8
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-07-24 10:56:32 +0200

    Perfect Numbers

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+public enum Classification {
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers {
+    public static Classification Classify(int number) {
+        var divisors = GetDivisors(number);
+        var sumOfDivisors = divisors.Sum();
+        if (sumOfDivisors == number) {
+            return Classification.Perfect;
+        }
+
+        if (sumOfDivisors > number) {
+            return Classification.Abundant;
+        }
+
+        return Classification.Deficient;
+    }
+
+    static int[] GetDivisors(int number)
+        => Enumerable.Range(1, number - 1).Where(n => number % n == 0).ToArray();
+}
