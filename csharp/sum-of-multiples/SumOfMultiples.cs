3f6cb275f9f4962b555251077c5e00de09f872e6
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-07-24 21:44:57 +0200

    Sum Of Multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples {
+    public static int Sum(IEnumerable<int> multiples, int max)
+        => Enumerable.Range(1, max - 1).Where(num => multiples.Any(divisor => num % divisor == 0)).Sum();
+}
