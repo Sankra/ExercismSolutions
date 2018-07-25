a675de31e8e19632d3a4e8034b40f3ad9b7bac76
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-07-25 09:44:28 +0200

    Armstrong Numbers

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers {
+    public static bool IsArmstrongNumber(int number) {
+        var digits = number
+            .ToString()
+            .ToCharArray()
+            .Select(c => (int)char.GetNumericValue(c))
+            .ToArray();
+        return digits.Sum(d => (int)Math.Pow(d, digits.Length)) == number;
+    }
+}
