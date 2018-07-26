d6a028cb99adf1755d50304c743e9a594195b397
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-07-26 14:59:31 +0200

    Binary Search

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinarySearch {
+    readonly List<int> input;
+
+    public BinarySearch(int[] input) {
+        this.input = new List<int>(input);
+        this.input.Sort();
+    }
+
+    public int Find(int value) {
+        var left = 0;
+        var right = input.Count - 1;
+        while (left <= right) {
+            var center = (int)Math.Floor((left + right) / 2D);
+            if (input[center] < value) {
+                left = center + 1;
+            } else if (input[center] > value) {
+                right = center - 1;
+            } else {
+                return center;
+            }
+        }
+
+        return -1;
+    }
+}
